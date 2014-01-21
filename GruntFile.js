edc47ed89c2a0a19660a304ac45e1004ac7bebb4
Rocco Janse <roccojanse@msn.com>
2014-01-21 11:53:49 +0100

    added sass and concat grunt plugins

--- GruntFile.js
+++ GruntFile.js
@@ -1,9 +1,32 @@
+'use strict';
+
 module.exports = function(grunt) {
 	// Do grunt-related things in here
 
 	// config
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')/*,
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {                          
+					style: 'expanded'
+				},
+				files: {
+					'www/css/<%= pkg.name %>-<%= pkg.version %>.css': ['src/scss/main.scss']
+				}
+			}
+		},
+
+		concat: {
+			// options: {
+			// 	separator: '\n',
+			// },
+			dist: {
+				src: ['src/js/intro.js', 'src/js/base.js', 'src/js/outro.js'],
+				dest: 'www/js/<%= pkg.name %>.js',
+			}
+		}/*,
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> *//*\n'
@@ -15,8 +38,15 @@ module.exports = function(grunt) {
 		}*/
 	});
 
+	// load plugins
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+	// define task(s)
+	grunt.registerTask('default', ['sass', 'concat']);
+
 	// A very basic default task.
-	grunt.registerTask('default', 'Log some stuff.', function() {
-		grunt.log.write('Logging some stuff...').ok();
-	});
+	// grunt.registerTask('default', 'Log some stuff.', function() {
+	// 	grunt.log.write('Logging some stuff...').ok();
+	// });
 };
