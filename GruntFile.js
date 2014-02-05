27cb0683c3cd837c8d93df72492715c056dda54b
Rocco Janse <roccojanse@msn.com>
2014-02-05 10:05:14 +0100

    restructured and added grunt tasks (jshint and jsdoc)

--- GruntFile.js
+++ GruntFile.js
@@ -3,15 +3,20 @@
 module.exports = function(grunt) {
 	
 	var jsFiles = [
-		'src/js/raf.polyfill.js',
-		'src/js/AssetManager.js',
-		'src/js/ObjectManager.js',
+		'src/js/polyfills/raf.polyfill.js',
+		'src/js/polyfills/hand-1.3.5.js',
+		'src/js/gfw/GFW.js',
+		'src/js/gfw/AssetManager.js',
+		'src/js/gfw/ObjectManager.js',
+		'src/js/gfw/Object.js',
+		'src/js/gfw/Text.js',
+		'src/js/gfw/Sprite.js',
 		'src/js/Game.js' 
 	];
 	
 	var jsConcatFiles = jsFiles.concat();
-	jsConcatFiles.unshift('src/js/intro.js');
-	jsConcatFiles.push('src/js/outro.js');
+	jsConcatFiles.unshift('src/js/tpl/intro.js');
+	jsConcatFiles.push('src/js/tpl/outro.js');
 
 
 	// config
@@ -31,7 +36,7 @@ module.exports = function(grunt) {
 
 		concat: {
 			// options: {
-			// 	separator: '\n',
+			//		separator: '\n',
 			// },
 			dist: {
 				src: jsConcatFiles,
@@ -45,17 +50,23 @@ module.exports = function(grunt) {
 			files: ['www/js/<%= pkg.name %>.js'],
 
 			options: {
-
 				undef: true,
 				unused: true,
+				loopfunc: true,
+				browser: true,
+				devel: true,
+				jquery: true
+			}
+		},
 
-				globals: {
-					jQuery: true,
-					console: true,
-					module: true,
-					document: true,
-					$: true,
-					window: true
+		jsdoc : {
+			dist : {
+				src: ['src/js/gfw/*.js', 'README.md'], 
+				//src: ['www/js/<%= pkg.name %>.js', 'README.md'],
+				options: {
+					destination: 'dist/docs',
+					//template: '',
+					private: true
 				}
 			}
 		}
@@ -66,8 +77,11 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jsdoc');
 
 	// define task(s)
-	grunt.registerTask('default', ['sass', 'concat', 'jshint']);
+	grunt.registerTask('default', ['sass', 'concat']);
+	grunt.registerTask('lint', ['sass', 'concat', 'jshint']);
+	grunt.registerTask('dist', ['sass', 'concat', 'jshint', 'jsdoc']);
 
 };
